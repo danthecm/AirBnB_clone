0247105c650912621c97f5268d65c88ee48ae274
danthecm <etchiedaniel1@gmail.com>
2023-03-19 00:52:17 +0100

    feat: task 4 => updated init method and accept args and kwargs then creates model from kwargs if kwargs exist

--- models/base_model.py
+++ models/base_model.py
@@ -12,13 +12,21 @@ class BaseModel:
     to create, update and manipulate models
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         Initialization method for the base model class
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if len(kwargs):
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                elif key == "created_at" or key == "updated_at":
+                    value = datetime.fromisoformat(value)
+                setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """
@@ -44,3 +52,27 @@ class BaseModel:
         dict_rep["created_at"] = str(self.created_at.isoformat())
         dict_rep["updated_at"] = str(self.updated_at.isoformat())
         return dict_rep
+
+
+my_model = BaseModel()
+my_model.name = "My_First_Model"
+my_model.my_number = 89
+print(my_model.id)
+print(my_model)
+print(type(my_model.created_at))
+print("--")
+my_model_json = my_model.to_dict()
+print(my_model_json)
+print("JSON of my_model:")
+for key in my_model_json.keys():
+    print("\t{}: ({}) - {}".format(key,
+          type(my_model_json[key]), my_model_json[key]))
+
+print("--")
+my_new_model = BaseModel(**my_model_json)
+print(my_new_model.id)
+print(my_new_model)
+print(type(my_new_model.created_at))
+
+print("--")
+print(my_model is my_new_model)
