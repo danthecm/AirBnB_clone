7ee987b1711159c4b04e55cfa0cc2f5e5e2d2dbf
danthecm <etchiedaniel1@gmail.com>
2023-03-18 23:01:51 +0100

    feat: task 3 => added doc string to str and init method

--- models/base_model.py
+++ models/base_model.py
@@ -13,11 +13,17 @@ class BaseModel:
     """
 
     def __init__(self):
+        """
+        Initialization method for the base model class
+        """
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
     def __str__(self):
+        """
+        String representation of the base model class
+        """
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
