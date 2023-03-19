6b8b38bd8946f4f604b188ebb91800c1ceed13f9
danthecm <etchiedaniel1@gmail.com>
2023-03-19 01:40:53 +0100

    feat: task 5 => add storage to the base model

--- models/__init__.py
+++ models/__init__.py
@@ -2,3 +2,7 @@
 """
 Init file for the models containing base_models.py
 """
+from models.engine import file_storage
+
+storage = file_storage.FileStorage()
+storage.reload()
--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 from datetime import datetime
 import uuid
+from models import storage
 """
 This Module contains a base model class
 """
@@ -27,6 +28,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         """
@@ -40,6 +42,7 @@ class BaseModel:
         the updated_at timestamp
         """
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """
