1623029045499d2cdc11ebd97ec984a4193a2993
danthecm <etchiedaniel1@gmail.com>
2023-04-12 00:05:39 +0100

    fix: fixing checker errors

--- console.py
+++ console.py
@@ -33,6 +33,7 @@ class HBNBCommand(cmd.Cmd):
         else:
             new_model = base_model.BaseModel()
             new_model.save()
+            return new_model.id
 
     def do_show(self, line):
         """
