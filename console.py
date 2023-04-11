d51286c096c969b2adc8a014a5c106ad035ed450
danthecm <etchiedaniel1@gmail.com>
2023-04-11 08:37:10 +0100

    feat: created a simple command line interpreter for airbnb console

---
+++ console.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+A command line module for interacting with
+the AirBnB Application
+"""
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    prompt = "(hbnb)"
+
+    def do_EOF(self, args):
+        """
+        Exists the program by returning True
+        if the user uses Ctrl+Z
+        """
+        return True
+
+    def do_quit(self, args):
+        """
+        Exists the program by returning True
+        if the user types quit and press enter
+        """
+        return True
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
