93d2e9c649f514c76762617f45461f60dc5d41d3
danthecm <etchiedaniel1@gmail.com>
2023-04-13 15:29:30 +0100

    feat: made console work with class name and methods like count, show,update, destroy

--- console.py
+++ console.py
@@ -6,14 +6,15 @@ the AirBnB Application
 import cmd
 from models import (base_model, storage, user, state,
                     city, place, amenity, review)
+import re
 
 
 class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
     class_list = {
         "BaseModel": base_model.BaseModel, "User": user.User,
-        "State": state.State, "City": city.City, "Place": place.Place, "Amenity": amenity.Amenity,
-        "Review": review.Review}
+        "State": state.State, "City": city.City, "Place": place.Place,
+        "Amenity": amenity.Amenity, "Review": review.Review}
 
     def emptyline(self):
         pass
@@ -127,6 +128,47 @@ class HBNBCommand(cmd.Cmd):
                 storage.save()
             else:
                 print("** no instance found **")
+
+    def do_count(self, line):
+        """
+        Creates a new instance of the given
+        class name
+        """
+        words = line.split(" ", 2)
+        if words[0] == "":
+            print("** class name missing **")
+        elif words[0] not in self.class_list:
+            print("** class doesn't exist **")
+        else:
+            all_models = storage.all()
+            my_model = [model for model in all_models.values(
+            ) if model.__class__.__name__ == words[0]]
+            print(len(my_model))
+
+    def default(self, line: str) -> None:
+        action_list = {"show": self.do_show, "all": self.do_all, "count": self.do_count,
+                       "update": self.do_update, "destroy": self.do_destroy}
+        words = line.split(".")
+        try:
+            model = words[0]
+            action = words[1]
+            class_name = self.class_list.get(model)
+            if not class_name:
+                return super().default(line)
+            start_param = action.index("(")
+            end_param = action.index(")")
+            actual_word = action[:start_param]
+            if actual_word not in action_list:
+                return super().default(line)
+            params = action[start_param + 1: end_param]
+            do_action = action_list.get(actual_word)
+            space_rep = [",", "'", '"']
+            clean_param = params
+            for i in range(len(space_rep)):
+                clean_param = clean_param.replace(space_rep[i], "")
+            do_action(model + " " + clean_param)
+        except Exception as e:
+            return super().default(line)
 
     do_EOF = do_quit
 
